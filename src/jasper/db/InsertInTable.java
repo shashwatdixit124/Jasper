d788f9fb2b20737f6f4bc6b9c48ceaafab52198a
Shashwat Dixit <shashwatdixit124@gmail.com>
2017-04-19 00:21:23 +0530

    fix : check for a default value to insert instead of null everytime

--- src/jasper/db/InsertInTable.java
+++ src/jasper/db/InsertInTable.java
@@ -82,10 +82,16 @@ public class InsertInTable extends HttpServlet {
 					String is_nullable = rs.getString("IS_NULLABLE");
 					String value = request.getParameter(name);
 					String data_type = rs.getString("DATA_TYPE");
+					String col_default = rs.getString("COLUMN_DEFAULT");
 					
 					if (value != null) {
 						if(!is_nullable.equalsIgnoreCase("NO") && value.isEmpty())
-							query = query + "NULL" + ", ";
+						{
+							if(!col_default.equals("NULL"))
+								query = query + "'"+col_default+"'" + ", ";
+							else
+								query = query + "NULL" + ", ";
+						}
 						else if(data_type.equals("tinyint"))
 							query = query + value + ", ";
 						else
